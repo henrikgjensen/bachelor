3ae939f17546f3fe9801c52a3d96abf24cc7753f
Michael Andersen <michael@diku.dk>
2009-10-09 15:33:13 +0000

    updated PubmedParser.py, and started to make the SearchTermCombiner module, which proves harder than first though.

---
+++ PubMedParser/src/SearchTermCombiner.py
@@ -0,0 +1,23 @@
+
+
+def searchTermCombiner(listOfSearchTerms):
+
+    # We might consider making this a class.
+
+    """
+    This modules is used to generate search terms to be used with the Pubmed[SearchEngine|Parser|Something], and different combinations of these. This is to help reduce the amount of returned articles. This is made seperately to be able to change the way it works more easily.
+    """
+    combineSearchTermList = listOfSearchTerms
+
+    for searchTerm in listOfSearchTerms:
+        
+    
+
+    # Might consider remove reversed searchterm, as Pubmed does not distincguise these. E.g. Myostoma Cyst returns the same as Cyst Myostoma (Or do they?) <-- These are fictious names.
+
+def combine(a, b):
+    return a + ' ' + b
+
+def removeDuplicateSearchTerms():
+
+    print 'test'
