cc5b8855784d82879a60942d7f986a9f19866f2c
Michael Andersen <bp@bp-laptop.(none)>
2009-11-19 13:53:15 +0100

    added support for writing out modules in IOmodule.py, in our case for writing out term list, pmid list and matrix containing count for the terms.

--- PubMedParser/src/IOmodule.py
+++ PubMedParser/src/IOmodule.py
@@ -1,5 +1,6 @@
 import os.path
 import os
+import pickle
 
 def writeOutTxt(dir,filename,text,mode='w'):
 
@@ -26,3 +27,33 @@ def writeOutTxt(dir,filename,text,mode='w'):
     out.write(string)
     out.close()
 
+def writeOutTDM(dirname, filename, object):
+# writeOut Term Document Matrix
+# Where object = (Matrix, TermList, PmidList)
+
+# Actually object do not need to look like that, the object just needs
+# to be pickleable
+
+    """
+    Simple function for writing a term document matrix, and its
+    associated term list and pmid list. It writes to a dirname and a
+    filename, is these do not exists it creates them. If they do
+    exists it simply overwrites the file.
+    
+    The directory is created in the $HOME directory of the user on the
+    work station.
+
+    The format is pythons seriaized object via the pickle module.
+
+    """
+
+    path=os.getenv("HOME")+'/'
+    
+    if not os.path.isdir(path+dirname):
+        os.mkdir(path+dirname)
+
+    filepath=path+dirname+'/'+filename+'.btd' # binary term document matrix
+    print filepath
+    fd = open(filepath,'w')
+    pickle.dump(object,fd)
+    fd.close()
