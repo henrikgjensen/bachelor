0b823437ab74209a431394e446f1fa3258eb701e
Michael Andersen <bp@bp-laptop.(none)>
2009-11-25 15:41:44 +0100

    Updated WordCounter, now removes empty strings

--- PubMedParser/src/WordCounter.py
+++ PubMedParser/src/WordCounter.py
@@ -1,11 +1,16 @@
 from nltk import *
+from TextCleaner import sanitizeString
 
 
 def wc(pmid, string):
     
     ll=[pmid,[]]
 
-    fdist = FreqDist(word.lower() for word in string.split(' '))
+    # Get the regex pattern that sanitizeses strings.
+    p = sanitizeString()
+
+    # Sanitize and remove empty strings ''
+    fdist = FreqDist([word.lower() for word in p.sub(' ', string).split(' ') if word != ''])
 
     ll[1].extend(fdist.items())
 
