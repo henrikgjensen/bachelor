cfabbe89fb1571f89b926123c06c96e64b63e8ea
Henrik <henne@henne-laptop.(none)>
2009-11-26 17:17:11 +0100

    ..

--- PubMedParser/src/TermDoc.py
+++ PubMedParser/src/TermDoc.py
@@ -160,7 +160,7 @@ def createTermDoc(subMatrixDir,termDocDir,termHash,pmidHash,refreshHash=False):
     termDoc = sparse.lil_matrix(m,n)
 
     for file in files:
-        subMatrix=readInTDM(subMatrixDir, file):
+        subMatrix=readInTDM(subMatrixDir, file)
         for i,j,v in zip(subMatrix.row, subMatrix.col, subMatrix.data):
             m = subMatrix[i,0]
             n = subMatrix[0,j]
