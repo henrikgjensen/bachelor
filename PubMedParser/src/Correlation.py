ccb88e7aeee2352c7099ec957d467377ef64ef70
Henrik <henne@henne-laptop.(none)>
2009-11-29 17:36:05 +0100

    ..

---
+++ PubMedParser/src/Correlation.py
@@ -0,0 +1,20 @@
+import TextCleaner
+
+def calculateCorrelation(M,searchVector):
+
+
+    # Convert the sparse amtrix to a compressed-sparse-row matrix
+    M=M.tocsr()
+
+    # Sanitize the search vector and convert it to a list of terms
+    sanitizer=TextCleaner.sanitizeString()
+    searchVector=[term.lower() for term in sanitizer.sub(' ', searchVector).split(' ') if term!='']
+
+    print str(searchVector)
+
+
+    for pmid in range(len(M.shape[1])-1):
+
+        vector=M[pmid+1,1:]
+
+        print pmid
