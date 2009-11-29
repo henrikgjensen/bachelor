71f3e811e59f365d8f94e2bfafbace2d458d7a7a
Henrik <henne@henne-laptop.(none)>
2009-11-29 18:16:19 +0100

    ..

--- PubMedParser/src/Correlation.py
+++ PubMedParser/src/Correlation.py
@@ -1,20 +1,50 @@
 import TextCleaner
 
+# Path to main folder
+_path=os.getenv("HOME")+'/'
+# Term-doc directory
+_termDocDir="TermDoc"
+# Term- and PMID-hash directory
+_hashTablesDir="hashTables"
+# Term-hash table file
+_termHashTable="termHash.btd"
+# PMID-hash table file
+_pmidHashTable="pmidHash.btd"
+
+
 def calculateCorrelation(M,searchVector):
 
+    # Load hash-lists
+    termHash=_path+_hashTablesDir+"/"+_termHashTable
+    pmidHash=_path+_hashTablesDir+"/"+_pmidHashTable
+    termHashData=open(termHash)
+    pmidHashData=open(pmidHash)
+    termHashTable=cPickle.load(termHashData)
+    pmidHashTable=cPickle.load(pmidHashData)
 
-    # Convert the sparse amtrix to a compressed-sparse-row matrix
-    M=M.tolil()
+    # Convert the sparse amtrix to a compressed-sparse-column matrix
+    M=M.tocsc()
 
     # Sanitize the search vector and convert it to a list of terms
     sanitizer=TextCleaner.sanitizeString()
     searchVector=[term.lower() for term in sanitizer.sub(' ', searchVector).split(' ') if term!='']
 
-    print str(searchVector)
+    # Look up hashes for terms
+    hashedSearchTerms=[]
+    for term in searchVector:
+        try:
+            termHash=termHashTable[term]
+        except:
+            print "Did not locate",term
+            continue
+        hashedSearchTerms.append(termHash)
 
+    print "Search vector:",str(searchVector),". Corresponding hash:",str(hashedSearchTerms)
 
-    for pmid in range(M.shape[0]-1):
+    # Locate pmids containing the given terms
+    colIndices=[]
+    for term in hashedSearchTerms:
 
-        vector=M[pmid+1,1:]
+        colIndices.append(M[0,term])
 
-        print pmid
+        print term
