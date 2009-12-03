d658a1ada0ee7896a817f386b4ac3edc8e414646
Henrik <henne@henne-laptop.(none)>
2009-12-03 22:08:34 +0100

    nothing special to report

--- PubMedParser/src/CosineMeasure.py
+++ PubMedParser/src/CosineMeasure.py
@@ -81,7 +81,9 @@ def cosineMeasure(M_lil, M_csc, queryString):
     # Extract the relevant indices of the row-vectors (pmid-hashes)
     searchIndices,hashedSearchTerms = SearchTermDoc.extractRowIndices(M_csc, queryString)
     # Union the arrays to avoid searching each row more than once
+    print len(reduce(set,map(set,searchIndices)))
     searchIndices = reduce(set.union,map(set,searchIndices))
+    print len(searchIndices)
     #print searchIndices
     #searchIndices = set(searchIndices)
 
