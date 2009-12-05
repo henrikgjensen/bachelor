bf091a9332b7995a8d2ee9a84f7721da8286deb2
Henrik <henne@henne-laptop.(none)>
2009-12-05 22:24:25 +0100

    Fixed paths and more

--- PubMedParser/src/FilterInterface.py
+++ PubMedParser/src/FilterInterface.py
@@ -18,13 +18,20 @@ _vectorLength = IOmodule.pickleIn(_hashTablePath,'CLHash')
 def generateLogTFIDF(M_coo):
 
     numberOfDocs = M_coo.shape[0]
-    #allHashedTerms = sorted(SearchTermDoc.termHashTable.values())
+    allHashedTerms = sorted(SearchTermDoc.termHashTable.values())
 
-    print "Converting from coo to lil..."
+
+    print "Transposing coo matrix"
     t1=time.time()
-    tfidfMatrix=M_coo.tolil()
+    MT_coo=M_coo.transpose()
     t2=time.time()
-    print "Matrix converted in",(t2-t1)
+    print "Matrix transposed in",(t2-t1)
+
+    print "Converting from (transposed) coo to lil..."
+    t1=time.time()
+    T_tfidfMatrix=MT_coo.tolil()
+    t2=time.time()
+    print "Matrix converted to lil in",(t2-t1)
 
 #    print "Making a csc_matrix format..."
 #    t1=time.time()
@@ -34,27 +41,27 @@ def generateLogTFIDF(M_coo):
 
 #    del M_coo
 
-#    print "Making a dok_matrix format..."
-#    t1=time.time()
-#    M_dok=M_coo.todok()
-#    t2=time.time()
-#    print "Made format in",(t2-t1)
+    for termVectorIndex in range(M_coo.shape[1]):
+        termVectorIndex += 1
+        print "Progress: " + str(len(allHashedTerms)-termVectorIndex)
+        #termVectorData = (M_csc.getcol(termVector).data)[1:]
+        docIndexVector = (T_tfidfMatrix.getrow(termVectorIndex).nonzero()[0])[1:]
+        # Calculate the inverse document frequency
+        # (Note that the length of each term vector is always greater than 0)
+        idf = math.log(numberOfDocs / len(docIndexVector))
 
-
-    for i,j,v in zip(M_coo.row, M_coo.col, M_coo.data):
-        m = tfidfMatrix[i,0] # row number = doc index
-        n = tfidfMatrix[0,j] # column number = term index
-        
-        idf = math.log(numberOfDocs / _vectorLength[n])
-        tf = math.log(1+v)
-
-        tfidfMatrix[m,n] = tf*idf
-
-        print "Coordinates: ("+str(m)+","+str(n)+")"
-
+        for docIndex in docIndexVector:
+            # Calculate the term frequency
+            tf = T_tfidfMatrix[termVectorIndex,docIndex]
+            if tf == 0:
+                print "Looked up zero-value at: ("str(termVectorIndex)+" "+str(docIndex)+")"
+                raise Exception
+            tf = math.log(1 + tf)
+            # Update the new matrix values
+            T_tfidfMatrix[termVectorIndex,docIndex] = tf * idf
 
     """
-    for termVectorIndex in range(tfidfMatrix.shape[1]):
+    for termVectorIndex in range(M_coo.shape[1]):
         termVectorIndex += 1
         print "Progress: " + str(len(allHashedTerms)-termVectorIndex)
         #termVectorData = (M_csc.getcol(termVector).data)[1:]
@@ -72,7 +79,10 @@ def generateLogTFIDF(M_coo):
             tf = math.log(1 + tf)
             # Update the new matrix values
             tfidfMatrix[docIndex, termVectorIndex] = tf * idf
+    """
+
+    tfidfMatrix=T_tfidfMatrix.transpose()
 
     IOmodule.writeOutTDM("/root/The_Hive/term_doc/tfidf_termDoc", "TFIDF_termdoc", tfidfMatrix)
-    """
+
 ###################
