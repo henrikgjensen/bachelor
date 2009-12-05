8a53d1e480514d36c0451e9b96b908273f5f0b0b
Henrik <henne@henne-laptop.(none)>
2009-12-05 17:34:22 +0100

    Fixed paths and more

--- PubMedParser/src/FilterInterface.py
+++ PubMedParser/src/FilterInterface.py
@@ -44,7 +44,7 @@ def generateLogTFIDF(M_coo):
         counter = 0
         termVector += 1
         print "Progress: " + str(len(allHashedTerms)-termVector)
-        termVector = (M_csc.getcol(termVector))[1:].data
+        termVector = (M_csc.getcol(termVector).data)[1:]
         # Calculate the inverse document frequency
         # (Note that the length of each term vector is always greater than 0)
         idf = math.log(numberOfDocs / len(termVector))
