5c32b9ca44b159b5af09b93072372964524024e1
Henrik <henne@henne-laptop.(none)>
2009-12-05 18:58:40 +0100

    Fixed paths and more

--- PubMedParser/src/FilterInterface.py
+++ PubMedParser/src/FilterInterface.py
@@ -11,25 +11,19 @@ def generateLogTFIDF(M_coo):
     numberOfDocs = len(SearchTermDoc.pmidHashTable)
     allHashedTerms = sorted(SearchTermDoc.termHashTable.values())
 
-    print "Duplicating matrix..."
-    t1=time.time()
-    tfidfMatrix=M_coo.copy()
-    t2=time.time()
-    print "Matrix duplicated in",(t2-t1)
-
     print "Converting from coo to lil..."
     t1=time.time()
-    tfidfMatrix=tfidfMatrix.tolil()
+    tfidfMatrix=M_coo.tolil()
     t2=time.time()
     print "Matrix converted in",(t2-t1)
 
-    del M_coo
-
     print "Making a csc_matrix format..."
     t1=time.time()
-    M_csc=tfidfMatrix.tocsc()
+    M_csc=M_coo.tocsc()
     t2=time.time()
     print "Made format in",(t2-t1)
+
+    del M_coo
 
     #print "Extracting term vectors"
     #t1=time.time()
@@ -41,7 +35,6 @@ def generateLogTFIDF(M_coo):
 
         
     for termVectorIndex in range(M_csc.shape[1]):
-        counter = 0
         termVectorIndex += 1
         print "Progress: " + str(len(allHashedTerms)-termVectorIndex)
         #termVectorData = (M_csc.getcol(termVector).data)[1:]
@@ -50,22 +43,15 @@ def generateLogTFIDF(M_coo):
         # (Note that the length of each term vector is always greater than 0)
         idf = math.log(numberOfDocs / len(docIndexVector))
 
-        print "Length of term vector before:", len(docIndexVector)
-
         for docIndex in docIndexVector:
-            counter += 1
-
-            if tfidfMatrix[docIndex, termVectorIndex] == 0:
-                print "Looked up zero-value at: "+str(docIndex)+" "+str(termVectorIndex)
-                raise Exception
-
             # Calculate the term frequency
             tf = tfidfMatrix[docIndex, termVectorIndex]
+            if tf == 0:
+                print "Looked up zero-value at: "+str(docIndex)+" "+str(termVectorIndex)
+                raise Exception
             tf = math.log(1 + tf)
             # Update the new matrix values
             tfidfMatrix[docIndex, termVectorIndex] = tf * idf
-
-        print "Length of term vector after (for the tfidf matrix):", counter
 
     IOmodule.writeOutTDM("/root/The_Hive/term_doc/tfidf_termDoc", "TFIDF_termdoc", tfidfMatrix)
 
