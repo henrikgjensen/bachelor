a6d34caee3f1741e2572ac0aa6d3693ee76c4fca
Henrik <henne@henne-laptop.(none)>
2009-12-09 12:18:35 +0100

    Fixed paths and more

--- PubMedParser/src/TFIDFMatrix.py
+++ PubMedParser/src/TFIDFMatrix.py
@@ -72,7 +72,7 @@ def test(M_lil,M_csr,M_coo):
 
         t3=time.time()
         for col in (M_lil.getrow(row).nonzero()[1])[1:]:
-            M_lil[row,col]=(M_coo[row,col])/norm
+            M_lil[row,col]=(M_lil[row,col])/norm
 
         #M_csr.getrow(row).data[1:] += norm
 
