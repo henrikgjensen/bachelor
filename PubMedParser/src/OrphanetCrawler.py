f040307540b92f4523e107e8b8068966a2ea1a09
Henrik <henne@henne-laptop.(none)>
2009-12-10 21:56:02 +0100

    Fixed paths and more

--- PubMedParser/src/OrphanetCrawler.py
+++ PubMedParser/src/OrphanetCrawler.py
@@ -58,7 +58,8 @@ def fetchOrphanetDiseaseTerms(pages):
 
     diseaseURLs={}
 
-    user_agent = 'Mozilla/5.0 (X11; U; Linux i686)'# Gecko/20071127 Firefox/2.0.0.11'
+    user_agent = 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;rv:1.9.0.4) Gecko/2008102920 Firefox/3.0.4'
     headers = { 'User-Agent' : user_agent }
 
     printvar=0
