8ab944c3a536569d32bacdfb0d29c2a15a1407e4
Henrik <henne@henne-laptop.(none)>
2009-12-14 16:31:20 +0100

    testing search20/30

--- PubMedParser/src/Search.py
+++ PubMedParser/src/Search.py
@@ -29,10 +29,40 @@ def search20(M_lil, M_csc, queryString, AND=False):
     results.sort()
     results.reverse()
 
-    top20=[result[1] for result in results[:19]]
+    top20=[result[1] for result in results[:20]]
 
     # Retrieve the top 20 results as PMIDs
     pmidList=SearchTermDoc.getPMIDList(top20)
+
+    pmidList=' '.join(pmid for pmid in pmidList)
+
+    return pmidList
+
+
+def search30(M_lil, M_csc, queryString, AND=False):
+
+    """
+    This function is still a work in progress..
+    """
+
+    # OPTIONAL:
+    # Stem the information
+    if _stemmer: queryString=FilterInterface.porterStemmer(queryString)
+
+    # CHOOSE HEURISTIC:
+    # Search-heuristic used to retrieve the list of results
+    results=SearchInterface.cosineMeasureOR(M_lil, M_csc, queryString)
+
+    if AND: results=SearchInterface.cosineMeasureAND(M_lil, M_csc, queryString)
+
+    # Sort the results and reverse to get the highest score first
+    results.sort()
+    results.reverse()
+
+    top30=[result[1] for result in results[:30]]
+
+    # Retrieve the top 20 results as PMIDs
+    pmidList=SearchTermDoc.getPMIDList(top30)
 
     pmidList=' '.join(pmid for pmid in pmidList)
 
