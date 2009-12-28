1e17cba13e74d97c7039091b4fe0579adba73c9c
Henrik <henne@henne-laptop.(none)>
2009-12-28 13:52:12 +0100

    Testing the new TFIDF matrix

--- PubMedParser/src/testModule.py
+++ PubMedParser/src/testModule.py
@@ -243,12 +243,30 @@ def go(MT_coo,MT_csr,M_lil,M_csc,M_coo):
     
     """
 
-import Image,ImageDraw
-def draw2d(data,labels,jpeg='mds2d.jpg'):
-    img=Image.new('RGB',(2000,2000),(255,255,255))
-    draw=ImageDraw.Draw(img)
-    for i in range(len(data)):
-        x=(data[i][0]+0.5)*1000
-        y=(data[i][1]+0.5)*1000
-        draw.text((x,y),labels[i],(0,0,0))
-    img.save(jpeg,'JPEG')
+from pylab import *
+
+def makehist():
+
+    
+    # Stemmed
+    x2 = [36,5,580,1134,39,77,570,21,35,47,196,137,3]
+    # Stemmed and tfidf-preprocessed
+    x3 = [14,7,1152,1011,277,49,42,3,16,0,1,13,4]
+
+    # the histogram of the data
+    n, bins, patches = hist(x, 50, normed=1)
+    setp(patches, 'facecolor', 'g', 'alpha', 0.75)
+
+    # add a 'best fit' line
+    y = normpdf(bins, mu, sigma)
+    l = plot(bins, y, 'r--')
+    setp(l, 'linewidth', 1)
+
+    xlabel('Smarts')
+    ylabel('Probability')
+    title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$')
+    axis([40, 160, 0, 0.03])
+    grid(True)
+
+    #savefig('histogram_demo',dpi=72)
+    show()
