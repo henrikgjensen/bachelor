9ce6f15d191c245c6b272b2957c303842af8a293
Michael Andersen <bp@bp-laptop.(none)>
2009-12-30 07:34:50 +0100

    First version of creatediseasematrix

---
+++ PubMedParser/src/CreateDiseaseMatrix.py
@@ -0,0 +1,46 @@
+import IOmodule as IO
+
+# Main folder
+_mainFolder=os.getenv("HOME")+"/"+"The_Hive"
+# Phase subfolder
+_subFolder = _mainFolder+"/"+"term_doc"
+# MedLine record directory
+_medlineDir=_mainFolder+"/data_acquisition/"+"medline_records"
+# Term-doc directory
+_termDocDir=_subFolder+"/"+"termDoc"
+# Term- and PMID-hash directory
+_hashTablesDir=_subFolder+"/"+"hashTables"
+# Disease label hash
+_labelHash="labelHash"
+
+
+def getRowIndices():
+
+    # Read through medline articles and get PMID to extract from
+    # complete term doc.
+
+    pmidHashTable=IOmodule.pickleIn(_hashTablesDir, _pmidHash)
+
+    listofpmids=[]
+    for i in range(len(f['records'])):
+        listofpmids.append(f['records'][field]['PMID'])
+
+    return listofpmids
+
+def createLabelVector():
+
+    # Sum over all columns and return a 1 x 456xxx vector
+
+    return labelVector
+
+def constructLabelMatrix():
+
+    listoffiles = getSortedFilelist('/root/The_Hive/data_acquisition/medline_records/'):    
+
+    listofpmids=[]
+    for f in listoffiles:
+        file_content = IO.evalIn(f)
+        
+        listofpmids=getRowIndices(file_content)
+
+    IO.writeOutTDM('/tmp', 'test')
