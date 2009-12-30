790e6b3351d7504b05a29f3af32a914bf1d997f5
Henrik <henne@henne-laptop.(none)>
2009-12-30 17:56:57 +0100

    Cleaning up the mess

--- PubMedParser/src/createSemanticSpace.py
+++ PubMedParser/src/createSemanticSpace.py
@@ -76,7 +76,7 @@ def _semanticSpace(U,Sig,Vt,reduce=90):
             break
 
     # Make sure there are at least 3 dimensions in the reduced matrix
-    if n>diagLen-3: n=diagLen-3
+    if n<diagLen-(diaglen-3): n=diagLen-(diaglen-3)
 
     print "Dimensions reduced: "+str(n)
     U=U[:,:-n]
