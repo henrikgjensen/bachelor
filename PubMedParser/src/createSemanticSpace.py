d925b8375abe3cfd96506a695c940d4a60222148
Henrik <henne@henne-laptop.(none)>
2009-12-30 20:29:09 +0100

    Cleaning up the mess

--- PubMedParser/src/createSemanticSpace.py
+++ PubMedParser/src/createSemanticSpace.py
@@ -86,7 +86,6 @@ def _semanticSpace(U,Sig,Vt,reduce=90):
         bottomUp=diagLen-i
 
         counter+=Sig[bottomUp,bottomUp]
-        print counter
 
         if counter >= percentReduce:
             n=i
