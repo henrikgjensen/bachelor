c221762aa480d4b7fda51ea13b1c0121e420622e
DIKU root <root@redqueen.ekstranet.diku.dk>
2010-01-02 20:09:17 +0100

    Some changes to the tfidfmatrix

--- PubMedParser/src/TFIDFMatrix.py
+++ PubMedParser/src/TFIDFMatrix.py
@@ -65,11 +65,11 @@ _termDocDir = _subFolder+"/"+"termDoc"
 #######################################################################################
 
  # TFIDF-matrix file name
-_tfidfName = "TFIDFMatrix_tfidf_stemmed"
+_tfidfName = "label_TFIDFMatrix"
  # Vector-norm hash for then TFIDFMatrix
 _RLHash = "RLHash_tfidf_stemmed"
  # Hash for the number of documents each term occur in
-_CLHash = "CLHash_tfidf_stemmed"
+_CLHash = "label_CLHash"
  # Load the precomputed length of each column in the stemmed term-doc matrix
 _termSum = IOmodule.pickleIn(_hashTablePath,_CLHash)
 
