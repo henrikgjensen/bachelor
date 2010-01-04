474db65748b37d498475e202e6b219eb73ddc343
Henrik <henne@henne-laptop.(none)>
2010-01-04 22:35:20 +0100

    Cosine playing

--- PubMedParser/src/createSemanticSpace.py
+++ PubMedParser/src/createSemanticSpace.py
@@ -176,6 +176,7 @@ def getSemanticKeywords(matrixDir,filename,top=20):
         term=revTermHashTable[M_csc[0,col]]
         termSum.append((sum(M_csc.getcol(col).data[:-1]),term))
 
-    termSum=sorted(termSum, key=lambda(k,v):(v,k), reverse=True)[:top]
+    termSum.sort()
+    termSum.reverse()
 
-    return termSum
+    return termSum[:top]
